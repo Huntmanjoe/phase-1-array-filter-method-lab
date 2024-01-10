ac312dca490dac7867bdaafbcfddc128aa874afc
Hunter <Huntermatyi@gmail.com>
2024-01-10 16:24:43 -0500

    donezo

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+
+const drivers = ["barry", "billy", "bobby", "robert"]
+
+function findMatching(array, string) {
+    return array.filter(element => element.toLowerCase() === string.toLowerCase());
+}
+
+console.log(findMatching(drivers, "barry")); 
+
+function fuzzyMatch(array, string) {
+    return array.filter(element => element.startsWith(string))
+
+}
+
+console.log(fuzzyMatch(drivers, "b"))
+
+
+const moreDrivers = [
+    {name: "david", hometown: "tottenville"},
+    {name: "daniel", hometown: "pleasant"},
+    {name: "josh", hometown: "grasmere"}
+]
+
+function matchName(driverObjects, string) {
+    return driverObjects.filter(element => element.name === string)
+
+}
+
+console.log(matchName(moreDrivers, "david"));
